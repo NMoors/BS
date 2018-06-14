ef0484475541b467a0e018f7b66eceeaaf92b74f
NMoors <nauka.moors@informatik.hs-fulda.de>
2018-06-14 11:12:19 +0200

    Fehler buf

--- bs53.c
+++ bs53.c
@@ -1,13 +1,13 @@
 #include "c4includes.h"
 
 char buf [128];
-int re, wr, status, pid, pfd[2];
+int re, wr, status, pid, rp, pfd[2];
 
 
 int main()
 {
 	 // 0 lesen 1 schreiben
-	pipe(pfd);
+	rp = pipe(pfd);
 	pid= fork();
 
 	if(pid == -1) {
@@ -16,17 +16,17 @@ int main()
 		return 1;
 	}
 	
-	if (pid == 1){
+	if (pid == 0){
 		//Kindprozess
-		printf("Kindprozess: %d", getpid());
+		printf("Kindprozess: %d\n", getpid());
 		close(pfd[1]);
 		while(1){
 			//von pp lesen
 			re = read(pfd[0], buf, sizeof(buf));
-			printf ("Kind ließt %d", re);
 			wr = write(fileno(stdout), buf, sizeof(buf));
-			if(re == 0 || strncmp("exit",buf, 4) == 0) {
-				printf("Kind terminiert");
+			printf ("Kind: %d\n", re);
+			if(re == 0 || strncmp(buf, "exit", 4) == 0) {
+				printf("Kind terminiert\n");
 				close(pfd[0]);
 				exit(0xAA);
 			}
@@ -34,19 +34,23 @@ int main()
 		}
 	}
 	//Fall 1
-	printf("Elternprozess %d", getpid());
-	close(pfd[0]);
+	
+	printf("Elternprozess %d\n", getpid());
+
 	while(1)
 	{
 		re =read(fileno(stdin), buf, sizeof(buf)); 
 		wr = write(pfd[1], buf, sizeof(buf));
-		if(re == 0 || strncmp("exit",buf, 4) == 0) {
-			printf("parent terminiert");
+		printf ("Parent: %d\n", wr);
+		if(re == 0 || strncmp(buf, "exit", 4) == 0) {
+			printf("parent terminiert\n");
 			close(pfd[1]);
 			exit(0XBB);
 		}
 	}
+	close(pfd[0]);
 	wait(&status);
-	printf("Kind & Parent terminieren");
+	printf("Kind & Parent terminieren\n");
 	return 1;
+
 }
